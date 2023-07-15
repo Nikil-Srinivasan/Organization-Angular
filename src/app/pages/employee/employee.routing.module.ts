a9abb61565a1228bf7e9d3e73b38a6857f844c9b
Sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2023-07-15 11:54:06 +0530

    employee dashboard

--- src/app/pages/employee/employee.routing.module.ts
+++ src/app/pages/employee/employee.routing.module.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee.component';
 import { AuthGuard } from 'src/app/guards/auth.guard';
 import { Role } from 'src/app/models/role';
+import { TaskComponent } from './task/task.component';
+import { TaskPendingComponent } from './task-pending/task-pending.component';
+import { TaskOngoingComponent } from './task-ongoing/task-ongoing.component';
 
 export const EmployeeRoutes: Routes = [
   {
     path: '',
     children: [
       {
-        path: '',
+        path: 'employees',
         component: EmployeeComponent,
         data: {
           title: 'Employee Page',
@@ -16,6 +19,36 @@ export const EmployeeRoutes: Routes = [
         },
         canActivate : [AuthGuard]
       },
+      {
+        path: 'tasks',
+        component: TaskComponent,
+        data: {
+          title: 'Employee Task',
+          roles : [Role.Employee],
+        },
+        canActivate : [AuthGuard],
+      },
+      {
+        path : 'tasks/pending',
+        component : TaskPendingComponent,
+        data: {
+          title: 'Pending Task',
+        },
+      },
+      {
+        path : 'tasks/ongoing',
+        component : TaskOngoingComponent,
+        data: {
+          title: 'Pending Task',
+        },
+      },
+      {
+        path : 'tasks/pending',
+        component : TaskPendingComponent,
+        data: {
+          title: 'Pending Task',
+        },
+      }
     ]
     },
 ];
