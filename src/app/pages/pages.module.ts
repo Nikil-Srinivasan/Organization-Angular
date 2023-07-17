3b43506d460e513688a957a0c86af47e8bbb99d6
Sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2023-07-17 07:09:03 +0530

    chart js

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutes } from './pages.routing.module';
 import { MaterialModule } from '../material.module';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { NgApexchartsModule } from 'ng-apexcharts';
 // icons
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
@@ -55,7 +54,6 @@ import { EmployeeDashboardComponent } from './dashboard/employee-dashboard/emplo
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    NgApexchartsModule,
     RouterModule.forChild(PagesRoutes),
     TablerIconsModule.pick(TablerIcons),
   ],
