7f490eb631c2a3663b5ef7280e2b62af1b3abb6f
Nikil-Srinivasan <nikil.s.cce@gmail.com>
2023-07-21 11:27:00 +0530

    Confirm Logout and 404 page

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -5,12 +5,13 @@ import {
   Input,
   ViewEncapsulation,
 } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Role } from 'src/app/models/role';
 import { EmployeetaskService } from 'src/app/services/EmployeeTaskService/employeetask.service';
 import { AuthenticationService, CredentialsService } from 'src/app/services/auth';
+import { ConfirmLogoutComponent } from 'src/app/shared/confirm-logout/confirm-logout.component';
 
 @Component({
   selector: 'app-header',
@@ -26,40 +27,50 @@ export class HeaderComponent {
 
   showFiller = false;
   showBellIcon = false;
-  newTaskCount : number;
+  newTaskCount: number;
 
 
   constructor(
     public dialog: MatDialog,
-    private authenticationService : AuthenticationService,
+    private authenticationService: AuthenticationService,
     private credentialsService: CredentialsService,
     private employeetaskService: EmployeetaskService,
-    private router : Router
-    ) {}
+    private router: Router
+  ) { }
 
-  onLogoutHandle()
-  {
-    this.authenticationService.logout();
+  onLogoutHandle(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '350px';
+
+    const dialogRef = this.dialog.open(ConfirmLogoutComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === true) {
+        this.authenticationService.logout().subscribe(() => {
+          // After logout is complete, navigate to the login page
+          this.router.navigate(['/authentication/login']);
+        });
+      }
+    });
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.checkUserRoleForNotification();
     this.employeetaskService.getTaskCount().subscribe(value => {
       this.newTaskCount = value;
     })
   }
 
-  checkUserRoleForNotification(){
+  checkUserRoleForNotification() {
     const user = this.credentialsService.userValue?.role;
 
-    if(user == Role.Employee ){
+    if (user == Role.Employee) {
       this.showBellIcon = true;
       const employeeId = this.credentialsService.userValue?.nameid;
-     
+
     }
   }
 
-  
+
 
 }
